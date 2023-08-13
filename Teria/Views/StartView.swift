3a56ae5f4594e601bd589f16bb1505d86239035b
손보석 <dya_only@sonboseog-ui-MacBookPro.local>
2023-08-13 13:04:29 +0900

    Design: start view title

--- Teria/Views/StartView.swift
+++ Teria/Views/StartView.swift
@@ -10,22 +10,25 @@ struct StartView: View {
             VStack {
                 Spacer()
 
-                Text("👋")
-                    .font(.system(size: 100))
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: -10, trailing: 0))
+                VStack(alignment: .center) {
+                    Text("👋")
+                        .font(.system(size: 100))
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: -10, trailing: 0))
 
-                LinearGradient(gradient: Gradient(colors: [Color(red: 255 / 255, green: 146 / 255, blue: 140 / 255), Color(red: 10 / 255, green: 132 / 255, blue: 255 / 255)]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .mask(
-                        VStack(alignment: .leading) {
-                            Text("iOS에서 쉽고 빠르게")
-                                .font(.system(size: 30.5, weight: .medium))
-                            Text("급식을 확인해요")
-                                .font(.system(size: 40, weight: .bold))
-                            Text("S c h o o l     M e a l      P r o j e c t")
-                        }
-                    )
-                    .frame(height: 100)
-
+                    LinearGradient(gradient: Gradient(colors: [Color(red: 255 / 255, green: 146 / 255, blue: 140 / 255), Color(red: 10 / 255, green: 132 / 255, blue: 255 / 255)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .mask(
+                            VStack(alignment: .center) {
+                                Text("iOS에서 쉽고 빠르게")
+                                    .font(.system(size: 30.5, weight: .medium))
+                                Text("급식을 확인하기")
+                                    .font(.system(size: 40, weight: .bold))
+                                Text("S c h o o l        M e a l        P r o j e c t")
+                                    .font(.system(size: 15))
+                            }
+                        )
+                        .frame(height: 100)
+                }
+                
                 Spacer()
 
                 LinearGradient(gradient: Gradient(colors: [Color(red: 255 / 255, green: 146 / 255, blue: 140 / 255), Color(red: 10 / 255, green: 132 / 255, blue: 255 / 255)]), startPoint: .topLeading, endPoint: .bottomTrailing)
